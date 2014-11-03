f0c32428cd709c772e137afbe0b5ec839766aa69
Albert Lyu <albertworld@gmail.com>
2014-11-03 10:47:14 -0600

    Add drop staging tables script

---
+++ drop_staging.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+import database_tasks as db
+
+if __name__ == "__main__":
+  localhost = "localhost"
+  database = "nbadb"
+  username = "postgres"
+  password = "password"
+  
+  conn = db.create_connection(localhost,database,username,password)
+  cursor = conn.cursor()
+  query = "SELECT DISTINCT table_schema FROM information_schema.tables WHERE table_schema LIKE 'staging_%'";
+  cursor.execute(query)
+  rows = cursor.fetchall()
+  for row in rows:
+    drop_schema = "DROP SCHEMA " + row[0] + " CASCADE;"
+    print(drop_schema)
+    cursor.execute(drop_schema)
+
+  conn.commit()
+  cursor.close()
+  conn.close()
